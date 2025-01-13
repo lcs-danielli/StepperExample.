a8c7d1f3b1664f7e2feabe59a295c9718a273b28
Daniel Li <danli@lcs.on.ca>
2025-01-13 08:49:20 -0500

    Initial Commit

---
+++ StepperExample./StepperExample_App.swift
@@ -0,0 +1,17 @@
+//
+//  StepperExample_App.swift
+//  StepperExample.
+//
+//  Created by 李泽宇 on 2025-01-13.
+//
+
+import SwiftUI
+
+@main
+struct StepperExample_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
